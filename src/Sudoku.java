20147711c36ffaa4d36cf9c7035bb03335beb545
KevinRomsom <kevin.romsom@gmail.com>
2016-10-01 14:45:45 +0200

    prachtig

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,6 +1,8 @@
 /**
  * Created by s158964 on 1-10-2016.
  */
+import java.util.ArrayList;
+
 public class Sudoku {
     private static final int SIZE = 9;     // size of the grid e.g. 9 -> 9x9
     private static final int DMAX = 9;     // max digit to be filled in e.g. 9
@@ -18,6 +20,7 @@ public class Sudoku {
     };
 
     int solutionnr = 0; //solution counter
+    ArrayList restoreemptyness = new ArrayList();
     void run() {
         //TODO starts the solving process.
         print();
@@ -28,37 +31,52 @@ public class Sudoku {
         //END TODO
         }
 
-    boolean givesConflict(int r, int  c, int d) {
+    boolean givesConflict(int r, int  c, int n) {
         //TODO is there a conflict when we fill in d at position r,c?
-
-
+        if(rowConflict(r, n) || colConflict(c, n) || boxConflict(r, c, n)){
+            return true;
+        }
 
         //END TODO
         return false;
     }
 
-    boolean rowConflict(int r, int d) {
+    boolean rowConflict(int r, int n) {
         //TODO is there a conflict in row r when we fill in d?
-
+        for (int i = 0; i < grid.length; i++){
+            if (grid[r][i] == n){
+                return true;
+            }
+        }
 
 
         //END TODO
         return false;
     }
 
-    boolean colConflict(int c, int d) {
+    boolean colConflict(int c, int n) {
         //TODO is there a conflict in column c when we fill in d?
-
+        for (int i = 0; i < grid.length; i++){
+            if(grid[i][c] == n){
+                return true;
+            }
+        }
 
 
         //END TODO
         return false;
     }
 
-    boolean boxConflict(int rr, int cc, int d) {
+    boolean boxConflict(int rr, int cc, int n) {
         //TODO is there a conflict when we fill in d at position in the box of rr,cc?
-
-
+        int[] array = locateBox(rr, cc);
+        for(int i = array[0]; i <= array[1]-array[0]; i++){
+            for(int j = array[2]; j <= array[3]-array[2]; j++){
+                if(grid[i][j] == n){
+                    return true;
+                }
+            }
+        }
 
         //END TODO
         return false;
@@ -67,8 +85,20 @@ public class Sudoku {
 
     int[] findEmptySquare() {
         //TODO return the next empty square (See assignment).
-
-
+        int rempty = 0;
+        int cempty = 0;
+        int[] emptyloc = new int[2];
+        for(int i = cempty; i < grid.length; i++){
+            for(int j = rempty; j < grid.length; j++){
+                if(grid[j][i] == 0){
+                    rempty = j;
+                    cempty = i;
+                    emptyloc[0] = j;
+                    emptyloc[1] = i;
+                    return emptyloc;
+                }
+            }
+        }
 
         //END TODO
         return null;
@@ -76,7 +106,17 @@ public class Sudoku {
 
     void solve() {
         //TODO see (4)
+        int[] coordinates = findEmptySquare();
 
+        for(int i = 1; i <= 9; i++){
+            if(givesConflict(coordinates[0], coordinates[1], i)){
+                continue;
+            }
+            else{
+                grid[coordinates[0]][coordinates[1]] = i;
+                solutionnr++;
+            }
+        }
 
 
         //END TODO
@@ -104,18 +144,84 @@ public class Sudoku {
                 }
                 counti++;
             } counti = 0;
+            System.out.println("|");
             if (countj == 2 || countj == 5){
-                System.out.print("-----------------------\n");
+                System.out.println("-----------------------");
             }
             countj++;
-            System.out.println("|");
+
         }countj = 0;
 
         //END TODO
     }
 
     //TODO extra methods
-
+    void int[] locateBox(int r, int c){
+        int[] location = new int [4];
+        if (0 <= r && r <= 2 && 0 <= c && c <= 2){
+            location[0] = 0;
+            location[1] = 2;
+            location[2] = 0;
+            location[3] = 2;
+            return location;
+        }
+        if (3 <= r && r <= 5 && 0 <= c && c <= 2){
+            location[0] = 3;
+            location[1] = 5;
+            location[2] = 0;
+            location[3] = 2;
+            return location;
+        }
+        if (6 <= r && r <= 8 && 0 <= c && c <= 2){
+            location[0] = 6;
+            location[1] = 8;
+            location[2] = 0;
+            location[3] = 2;
+            return location;
+        }
+        if (0 <= r && r <= 2 && 3 <= c && c <= 5){
+            location[0] = 0;
+            location[1] = 2;
+            location[2] = 3;
+            location[3] = 5;
+            return location;
+        }
+        if (3 <= r && r <= 5 && 3 <= c && c <= 5){
+            location[0] = 3;
+            location[1] = 5;
+            location[2] = 3;
+            location[3] = 5;
+            return location;
+        }
+        if (6 <= r && r <= 8 && 3 <= c && c <= 5){
+            location[0] = 6;
+            location[1] = 8;
+            location[2] = 3;
+            location[3] = 5;
+            return location;
+        }
+        if (0 <= r && r <= 2 && 6 <= c && c <= 8){
+            location[0] = 0;
+            location[1] = 2;
+            location[2] = 6;
+            location[3] = 8;
+            return location;
+        }
+        if (3 <= r && r <= 5 && 6 <= c && c <= 8){
+            location[0] = 3;
+            location[1] = 5;
+            location[2] = 6;
+            location[3] = 8;
+            return location;
+        }
+        if (6 <= r && r <= 8 && 6 <= c && c <= 8){
+            location[0] = 6;
+            location[1] = 8;
+            location[2] = 6;
+            location[3] = 8;
+            return location;
+        }
+    }
 
 
     //END TODO
